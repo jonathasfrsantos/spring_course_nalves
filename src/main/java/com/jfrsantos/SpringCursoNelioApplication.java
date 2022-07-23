3ceebbfb6f17a5841e42a228d2918ec963c68d6d
jonathasfrsantos <jonathasfrsantos@gmail.com>
2022-07-22 22:53:27 -0300

    Project created

---
+++ src/main/java/com/jfrsantos/SpringCursoNelioApplication.java
@@ -0,0 +1,13 @@
+package com.jfrsantos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCursoNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCursoNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfrsantos/SpringCursoNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfrsantos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCursoNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
